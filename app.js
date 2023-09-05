ed77e27cca5b6fcbb6dc5d718aeae40beae95066
Sefa Ağardan <54821580+g4stlyx@users.noreply.github.com>
2023-09-05 15:57:05 +0300

    docs added using apimatic and swagger ui

--- app.js
+++ app.js
@@ -9,6 +9,11 @@ const cors = require("cors")
 const xss = require("xss-clean")
 const rateLimiter = require("express-rate-limit")
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const connectDB = require('./db/connect');
 const authenticateUser = require("./middleware/authentication");
 // routers
@@ -31,6 +36,7 @@ app.use(xss())
 app.get('/', (req, res) => {
   res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
